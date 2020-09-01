2d1d17f9d7dc60cd5092c5373617cdc334e1c0a3
Alex <alexwarofcommand@gmail.com>
2020-09-01 15:25:30 +0300

    Updates

---
+++ precache-manifest.5190bad92368c998ef01bc6bd9ed47b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c0db1eb551182a24ae55c489510ee32",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "adf13bad9368fd3ad331",
+    "url": "/monster-rolodex/static/css/main.9288d228.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adf13bad9368fd3ad331",
+    "url": "/monster-rolodex/static/js/main.492271bd.chunk.js"
+  },
+  {
+    "revision": "d2a9c49a159018779ca4",
+    "url": "/monster-rolodex/static/js/runtime-main.131bb20d.js"
+  }
+]);
